e4f3200b38ae2cd58bf057afe1c1b3dfc147c7fb
jennifer <jennifer@jennifer-275E4E-275E5E>
2016-10-21 09:26:15 -0200

    Revisão Classe Dado. Adição de metodos de tratamente 2pl de transações em Escalonador

--- src/Escalonador/Tupla.java
+++ src/Escalonador/Tupla.java
@@ -16,12 +16,24 @@ import produtor.Dado;
  * @author jennifer
  */
 public class Tupla {
+
     private int idTransacao;
-    private String operacao;
-    private String dado;
-    private String timeStamp;
+    private Operacao operacao;
+    private Dado dado;
+    private Date timeStamp;
+
+    private String temp_operacao;
+    private String temp_dado;
+    private String temp_timeStamp;
 
     public Tupla(int idTransacao, String operacao, String dado, String timeStamp) {
+        this.idTransacao = idTransacao;
+        this.temp_operacao = operacao;
+        this.temp_dado = dado;
+        this.temp_timeStamp = timeStamp;
+    }
+
+    public Tupla(int idTransacao, Operacao operacao, Dado dado, Date timeStamp) {
         this.idTransacao = idTransacao;
         this.operacao = operacao;
         this.dado = dado;
@@ -36,36 +48,41 @@ public class Tupla {
         this.idTransacao = idTransacao;
     }
 
-    public String getOperacao() {
+    public Operacao getOperacao() {
         return operacao;
     }
 
-    public void setOperacao(String operacao) {
+    public void setTemp_operacao(String temp_operacao) {
+        this.temp_operacao = temp_operacao;
+    }
+
+    public void setOperacao(Operacao operacao) {
         this.operacao = operacao;
     }
 
-    public String getDado() {
+    public Dado getDado() {
         return dado;
     }
 
-    public void setDado(String dado) {
+    public void setTemp_dado(String temp_dado) {
+        this.temp_dado = temp_dado;
+    }
+
+    public void setDado(Dado dado) {
         this.dado = dado;
     }
 
-    public String getTimeStamp() {
+    public Date getTimeStamp() {
         return timeStamp;
     }
 
-    public void setTimeStamp(String timeStamp) {
-        this.timeStamp = timeStamp;
+    public void setTemp_timeStamp(String temp_timeStamp) {
+        this.temp_timeStamp = temp_timeStamp;
     }
 
     @Override
     public String toString() {
-        return "Tupla{" + "indice=" + idTransacao + ", operacao=" + operacao + ", dado=" + dado + ", timeStamp=" + timeStamp + '}';
+        return "Tupla{" + "indice=" + idTransacao + ", operacao=" + temp_operacao + ", dado=" + temp_dado + ", timeStamp=" + temp_timeStamp + '}';
     }
-    
-    
-    
-    
+
 }
